0a0c86c4d49fa1b348cc66e16c55c5219d59a525
Aadarsh <aadarsh-s@outlook.com>
2021-02-19 14:49:51 +0530

    Updates

---
+++ precache-manifest.b08fa1896232f91dd3e1683741df5d1f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94e9bcd391a3562d1d774e6bc142b36d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4a98dbc814149e24370",
+    "url": "/static/css/main.3aac88c1.chunk.css"
+  },
+  {
+    "revision": "909ae395ad1d1993306d",
+    "url": "/static/js/2.a8ebec4e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a8ebec4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4a98dbc814149e24370",
+    "url": "/static/js/main.ede42ceb.chunk.js"
+  },
+  {
+    "revision": "432d2e313baa75a278a4",
+    "url": "/static/js/runtime-main.e5b3960c.js"
+  },
+  {
+    "revision": "9a34037b4b151a13445544e4e4fc4fb7",
+    "url": "/static/media/bj.9a34037b.png"
+  },
+  {
+    "revision": "2f0551dfabe4a0a2f08bb3458cf5b23b",
+    "url": "/static/media/ecom.2f0551df.png"
+  },
+  {
+    "revision": "f24e3d29df530657dffeebb588d9ef25",
+    "url": "/static/media/job.f24e3d29.png"
+  },
+  {
+    "revision": "62186a57428f9b05d9ac53a02dbbbc8d",
+    "url": "/static/media/main.62186a57.jfif"
+  },
+  {
+    "revision": "2d232345e95a9e06e9fe0f281cecaa92",
+    "url": "/static/media/onestep.2d232345.jpeg"
+  },
+  {
+    "revision": "f612752aaeca4a58bd3e400aa5ee0a07",
+    "url": "/static/media/profile.f612752a.jpg"
+  },
+  {
+    "revision": "c5095cfdd9f85b57165e337e0209c098",
+    "url": "/static/media/rest.c5095cfd.jpg"
+  },
+  {
+    "revision": "922c239810b565af2afcb05129dc8059",
+    "url": "/static/media/seatbooked.922c2398.jpg"
+  }
+]);
